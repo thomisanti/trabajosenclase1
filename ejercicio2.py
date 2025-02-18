a55c5d594aa4d178a65700e1843901858ae80bfc
thomisanti <thomassantiago733@gmail.com>
2025-02-17 20:12:37 -0500

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,11 @@
+precio = float(input("Ingrese el precio del artículo: "))
+
+
+descuento = 0.15
+
+
+descuento_de_el_precio = (precio * descuento)
+
+precio_con_el_decuento = precio - descuento_de_el_precio 
+
+print("El precio final es:", precio_con_el_decuento)
