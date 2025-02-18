6e1b9331bf3fb96d419a239a956ce523c3e5b445
thomisanti <thomassantiago733@gmail.com>
2025-02-17 20:07:26 -0500

    Create ejercisio#1.cpp

---
+++ ejercisio#1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int num1;
+    int num2;
+    int suma;
+    int resta;
+    int multiplicasion;
+    float division;
+    float modulo;
+    cout<<"ingrese el numero";
+    cin>> num1;
+
+    cout<<"ingrese el segundo numero";
+    cin>> num2;
+
+    suma = num1 + num2;
+
+    cout<< suma << endl;
+    
+    resta = num1 - num2;
+    
+    cout<< resta << endl;
+    
+    multiplicasion = num1 * num2 ;
+    
+    cout<< multiplicasion<< endl;
+    
+    division = num1 / num2;
+    
+    cout<< division<< endl;
