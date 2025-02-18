f4555b459c428b32c01afb0c94ba9e7056fdc137
thomisanti <thomassantiago733@gmail.com>
2025-02-17 20:08:16 -0500

    Create ejercisio1.py

---
+++ ejercisio1.py
@@ -0,0 +1,20 @@
+#ejercisio numero 1 
+
+num1=int(input("dame el primer numero "))
+num2=int(input("dame el segundo numero"))
+
+suma = num1 + num2 
+print (f"la suma de las variables da {suma}")
+
+resta = num1 - num2 
+print (f"la resta de las variables es {resta}")
+
+multiplicacion = num1 * num2 
+print (f"la multiplicacion de las variables es {multiplicacion}")
+
+división= num1 / num2
+print (f"la division de las variables es {división}")
+
+
+modulo = num1 % num2  
+print(f"el modulo de las variables es {modulo}")
