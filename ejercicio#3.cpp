8ab56e1040de6078be26a26ad8c2cd676d911a22
thomisanti <thomassantiago733@gmail.com>
2025-02-17 20:26:16 -0500

    Create ejercicio#3.cpp

---
+++ ejercicio#3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+      int valor = 10;
+    
+
+    valor = valor + 5; 
+    
+
+    valor = valor - 3;  
+    
+
+    valor = valor * 2;  
+
+    double resultado = valor / 4.0;  
+    
+
+    cout << "El resultado final es: " << resultado << endl;
+    
+    return 0;
+    
+} 
